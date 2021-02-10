16f8887a62b4478f9bd698526f2d5f16026dc771
Sofien Moussa <smoussa@pcalb-mm0000.univ-jfc.fr>
2021-02-10 11:21:37 +0100

    ajout: sortie de l'app à la fermeture de la fenetre

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -1,6 +1,8 @@
 package tuto_git;
 
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Application extends Frame {
 
@@ -20,6 +22,13 @@ public class Application extends Frame {
 		this.add(vue);
 		this.pack();
 		this.setVisible(true);
+		
+		addWindowListener(new WindowAdapter(){  
+            public void windowClosing(WindowEvent e) {  
+                dispose();  
+            }  
+        });
+		
 	}
 
 }
